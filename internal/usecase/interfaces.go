b425191d489d7589b5f4286919f674b3cdea4faa
my-might <valeriakrivosheeva333@gmail.com>
2024-03-10 23:02:59 +0200

    auth done

--- internal/usecase/interfaces.go
+++ internal/usecase/interfaces.go
@@ -2,20 +2,54 @@ package usecase
 
 import (
 	"context"
+	"github.com/jackc/pgconn"
 	"github.com/jackc/pgx/v4"
-	"github.com/pets-shelters/backend-svc/internal/usecase/repo"
-
-	"github.com/pets-shelters/backend-svc/internal/entity"
+	"github.com/pets-shelters/backend-svc/internal/structs"
+	"github.com/pets-shelters/backend-svc/internal/structs/requests"
+	"github.com/pets-shelters/backend-svc/internal/usecase/repo/entity"
 )
 
 type (
-	Authorization interface {
-		Registration(context.Context, entity.Shelter, string) error
+	IConnection interface {
+		Exec(ctx context.Context, sql string, arguments ...interface{}) (commandTag pgconn.CommandTag, err error)
+		Query(ctx context.Context, sql string, args ...interface{}) (pgx.Rows, error)
+		QueryRow(ctx context.Context, sql string, args ...interface{}) pgx.Row
+		QueryFunc(ctx context.Context, sql string, args []interface{}, scans []interface{}, f func(pgx.QueryFuncRow) error) (pgconn.CommandTag, error)
 	}
 
-	DBRepo interface {
-		GetSheltersRepo() *repo.SheltersRepo
-		GetUsersRepo() *repo.UsersRepo
+	IDBRepo interface {
+		GetSheltersRepo() ISheltersRepo
+		GetUsersRepo() IUsersRepo
 		Transaction(ctx context.Context, f func(pgx.Tx) error) error
+	}
+
+	ISheltersRepo interface {
+		CreateWithConn(ctx context.Context, conn IConnection, shelter entity.Shelter) (int64, error)
+		Create(ctx context.Context, shelter entity.Shelter) (int64, error)
+		SelectSheltersWithConn(ctx context.Context, conn IConnection) ([]entity.Shelter, error)
+		SelectShelters(ctx context.Context) ([]entity.Shelter, error)
+	}
+
+	IUsersRepo interface {
+		CreateWithConn(ctx context.Context, conn IConnection, user entity.User) (int64, error)
+		Create(ctx context.Context, user entity.User) (int64, error)
+		SelectUsersWithConn(ctx context.Context, conn IConnection) ([]entity.User, error)
+		SelectUsers(ctx context.Context) ([]entity.User, error)
+		UpdateShelterIDWithConn(ctx context.Context, conn IConnection, userEmail string, shelterId int64) (int64, error)
+	}
+
+	IJwt interface {
+		CreateTokensPair(userEmail string) (*structs.TokensPair, error)
+		VerifyAccessToken(accessTokenString string) (string, error)
+		VerifyRefreshToken(refreshTokenString string) (string, error)
+	}
+
+	IAuthorization interface {
+		Login() (*structs.LoginResult, error)
+		Callback(ctx context.Context, cookie string, googleState string, googleCode string) (*structs.TokensPair, error)
+	}
+
+	IShelters interface {
+		Create(ctx context.Context, req requests.CreateShelter, userEmail string) error
 	}
 )
